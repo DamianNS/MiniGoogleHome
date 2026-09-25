12efa6ab06136f6c19a34982731f5d671ae9c3fb
Damián Saint Paul <53456589+DamianNS@users.noreply.github.com>
2026-09-25 11:20:55 -0300

    api/devices Authorize

--- SmartHome.Backend/Controllers/DevicesController.cs
+++ SmartHome.Backend/Controllers/DevicesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SmartHome.Shared.Entities;
@@ -8,6 +9,7 @@ namespace SmartHome.Backend.Controllers
 {
     [Route("api/devices")]
     [ApiController]
+    [Authorize]
     public class DevicesController(IDbContextFactory<SmartHomeDbContext> dbContextFactory) : ControllerBase
     {
         [HttpGet]
